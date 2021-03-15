855f6aa6e3e9c38422bb6cfc8478f37863a15195
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-03-15 19:21:41 +0000

    Numba parallelism added

--- ibm_qiskit/utils/transforms/QiskitQuantumHadamardTransform.py
+++ ibm_qiskit/utils/transforms/QiskitQuantumHadamardTransform.py
@@ -12,6 +12,17 @@ Acknowledgments:
 - Paulo Alexandre Mateus (pmat@math.ist.utl.pt)
 """
 
+# Import Libraries and Packages
+
+# Import NoPython mode of Just-In-Time and Parallel Range from Numba
+from numba import jit, prange
+
+# Import N-Dimensional Arrays from NumPy
+from numpy import array
+
+# Flag of parallelization to specify the parallelization parameter of Numba
+USE_NUMBA_PARALLEL = True
+
 
 # Class for the IBM Qiskit's Quantum Hadamard Transform
 class QiskitQuantumHadamardTransform:
@@ -24,10 +35,13 @@ class QiskitQuantumHadamardTransform:
 
     # Apply the Quantum Hadamard Transform to
     # the respective given Qubits' indexes in the IBM Qiskit's Quantum Circuit
+    # Note: Change the Flag of parallelization "USE_NUMBA_PARALLEL" to
+    #       specify the parallelization parameter of Numba
+    @jit(nopython=False, forceobj=True, parallel=USE_NUMBA_PARALLEL)
     def apply_transform(self):
 
         # For each indexed Qubit
-        for qubit_index in self.qubits_indexes:
+        for qubit_index in prange(array(self.qubits_indexes).shape[0]):
 
             # Apply the Hadamard Gate to the current Qubit index
             self.quantum_circuit.apply_hadamard(qubit_index)
