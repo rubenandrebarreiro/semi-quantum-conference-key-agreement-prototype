ec902f8c296c32c64e636247e2b12cee1527c9ff
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-03-23 18:05:02 +0000

    IBM Qiskit's Party for the Semi-Quantum Conference Key Agreement (SQCKA) Protocol added

---
+++ src/ibm_qiskit/cryptography/semi_quantum_conference_key_agreements/common/QiskitSQCKAProtocolParty.py
@@ -0,0 +1,34 @@
+"""
+Semi-Quantum Conference Key Agreement (SQCKA)
+
+Author:
+- Ruben Andre Barreiro (r.barreiro@campus.fct.unl.pt)
+
+Supervisors:
+- Andre Nuno Souto (ansouto@fc.ul.pt)
+- Antonio Maria Ravara (aravara@fct.unl.pt)
+
+Acknowledgments:
+- Paulo Alexandre Mateus (pmat@math.ist.utl.pt)
+"""
+
+
+# Class for IBM Qiskit's Semi-Quantum Conference Key Agreement (SQCKA) Protocol Party
+class QiskitSQCKAProtocolParty:
+
+    # Constructor for IBM Qiskit's Semi-Quantum Conference Key Agreement (SQCKA) Party
+    def __init__(self, party_name, master_status):
+
+        # Set the Party's name
+        self.party_name = party_name
+
+        # Set the boolean flag, responsible to keep the information about if the Party is
+        # the Master of the Protocol or not
+        self.master_status = master_status
+
+    # Return the boolean flag, responsible to keep the information about if the Party is
+    # the Master of the Protocol or not
+    def is_master(self):
+
+        return self.master_status
+
