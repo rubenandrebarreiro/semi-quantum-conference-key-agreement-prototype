d278686c2b73fa3b1eb09dd497575d175cbb4a3b
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-03-29 15:57:07 +0100

    New Parameters for the IBM's Qiskit added

---
+++ src/ibm_qiskit/common/QiskitLibraryParameters.py
@@ -0,0 +1,17 @@
+"""
+Semi-Quantum Conference Key Agreement (SQCKA)
+
+Author:
+- Ruben Andre Barreiro (r.barreiro@campus.fct.unl.pt)
+
+Supervisors:
+- Andre Nuno Souto (ansouto@fc.ul.pt)
+- Antonio Maria Ravara (aravara@fct.unl.pt)
+
+Acknowledgments:
+- Paulo Alexandre Mateus (pmat@math.ist.utl.pt)
+"""
+
+# The maximum number of Qubits for the QASM Simulator of the IBM's Qiskit Library
+QISKIT_LIBRARY_QASM_SIMULATOR_MAX_NUM_QUBITS = 32
+
