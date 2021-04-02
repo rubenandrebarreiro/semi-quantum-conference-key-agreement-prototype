cbd0e132622edd5687ef0b53516db358f5080ea5
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-04-02 18:52:46 +0100

    Enumeration for Memory Module Types added

---
+++ src/ibm_qiskit/common/MemoryModuleTypes.py
@@ -0,0 +1,24 @@
+"""
+Semi-Quantum Conference Key Agreement (SQCKA)
+
+Author:
+- Ruben Andre Barreiro (r.barreiro@campus.fct.unl.pt)
+
+Supervisors:
+- Andre Nuno Souto (ansouto@fc.ul.pt)
+- Antonio Maria Ravara (aravara@fct.unl.pt)
+
+Acknowledgments:
+- Paulo Alexandre Mateus (pmat@math.ist.utl.pt)
+"""
+
+# The Enumerations and Constants
+
+# The tag enumeration for Hybrid Memory
+HYBRID_HARDWARE_ENUM = "HYBRID_MEMORY"
+
+# The tag enumeration for Quantum Memory
+QUANTUM_MEMORY_ENUM = "QUANTUM_MEMORY"
+
+# The tag enumeration for Classical Memory
+CLASSICAL_MEMORY_ENUM = "CLASSICAL_MEMORY"
