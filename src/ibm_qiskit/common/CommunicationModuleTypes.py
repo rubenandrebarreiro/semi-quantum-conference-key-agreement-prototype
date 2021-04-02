69897b375668dbc8c159a953bbaf4b2af6e4f0cc
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-04-02 18:52:54 +0100

    Enumeration for Communication Module Types added

---
+++ src/ibm_qiskit/common/CommunicationModuleTypes.py
@@ -0,0 +1,27 @@
+"""
+Semi-Quantum Conference Key Agreement (SQCKA)
+
+Author:
+- Ruben Andre Barreiro (r.barreiro@campus.fct.unl.pt)
+
+Supervisors:
+- Andre Nuno Souto (ansouto@fc.ul.pt)
+- Antonio Maria Ravara (aravara@fct.unl.pt)
+
+Acknowledgments:
+- Paulo Alexandre Mateus (pmat@math.ist.utl.pt)
+"""
+
+# The Enumerations and Constants
+
+# The tag enumeration for Quantum Communication with Discrete Variables
+QUANTUM_COMMUNICATION_DISCRETE_VARIABLES_ENUM = "QUANTUM_COMMUNICATION_DISCRETE_VARIABLES"
+
+# The tag enumeration for Quantum Communication with Continuous Variables
+QUANTUM_COMMUNICATION_CONTINUOUS_VARIABLES_ENUM = "QUANTUM_COMMUNICATION_CONTINUOUS_VARIABLES"
+
+# The tag enumeration for Quantum Communication
+QUANTUM_COMMUNICATION_ENUM = "QUANTUM_COMMUNICATION"
+
+# The tag enumeration for Classical Communication
+CLASSICAL_COMMUNICATION_ENUM = "CLASSICAL_COMMUNICATION"
