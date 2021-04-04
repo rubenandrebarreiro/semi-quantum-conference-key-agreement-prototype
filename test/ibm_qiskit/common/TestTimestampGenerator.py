57ff2009cd1fda58d9fa218c656957c6224b2bde
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-04-04 23:51:00 +0100

    More new Unitary Tests for the Timestamp Generator added

--- test/ibm_qiskit/common/TestTimestampGenerator.py
+++ test/ibm_qiskit/common/TestTimestampGenerator.py
@@ -26,8 +26,155 @@ class TimestampPseudoRandomGeneratorTests(unittest.TestCase):
 
     # Test #1 for Generation of Timestamps
     # Description of the Test Case:
+    # - Generate the current now Date Time;
+    def test_generate_now(self):
+
+        # Print the Heading Logging for the Test #1
+        print("--- Test #1 for Generation of Timestamps ---")
+
+        # Create the Timestamp's Generator
+        timestamp_generator = TimestampGenerator.TimestampGenerator("timestamp_generator")
+
+        # Print the current now Date Time
+        print("Current Now Date Time: ", timestamp_generator.get_now())
+
+        # Print one Blank Line, for clear Logging
+        print("\n")
+
+        # Dummy Assert Equal for Unittest
+        self.assertEqual(True, True)
+
+    # Test #2 for Generation of Timestamps
+    # Description of the Test Case:
+    # - Generate the current now Date Time, as customised format;
+    def test_generate_now_customised_format(self):
+
+        # Print the Heading Logging for the Test #2
+        print("--- Test #2 for Generation of Timestamps ---")
+
+        # Create the Timestamp's Generator
+        timestamp_generator = TimestampGenerator.TimestampGenerator("timestamp_generator")
+
+        # Print the current now Date Time
+        print("Current Now Date Time, in customised format: ",
+              timestamp_generator.get_now_customised_format())
+
+        # Print one Blank Lines, for clear Logging
+        print("\n")
+
+        # Dummy Assert Equal for Unittest
+        self.assertEqual(True, True)
+
+    # Test #3 for Generation of Timestamps
+    # Description of the Test Case:
+    # - Generate the current now Date Time, as Timestamp format;
+    def test_generate_now_timestamp(self):
+
+        # Print the Heading Logging for the Test #3
+        print("--- Test #3 for Generation of Timestamps ---")
+
+        # Create the Timestamp's Generator
+        timestamp_generator = TimestampGenerator.TimestampGenerator("timestamp_generator")
+
+        # Print the current now Date Time, in Timestamp format
+        print("Current Now Date Time, in Timestamp format: ",
+              timestamp_generator.get_now_timestamp())
+
+        # Print one Blank Lines, for clear Logging
+        print("\n")
+
+        # Dummy Assert Equal for Unittest
+        self.assertEqual(True, True)
+
+    # Test #4 for Generation of Timestamps
+    # Description of the Test Case:
+    # - Generate the current now Date Time, plus a Delta value;
+    def test_generate_now_plus_delta(self):
+
+        # Print the Heading Logging for the Test #4
+        print("--- Test #4 for Generation of Timestamps ---")
+
+        # Create the Timestamp's Generator
+        timestamp_generator = TimestampGenerator.TimestampGenerator("timestamp_generator")
+
+        # Print the current now Date Time
+        print("Current Now Date Time, plus a Delta (30 minutes) value: ",
+              timestamp_generator.get_now_plus_delta(weeks_delta=0,
+                                                     days_delta=0,
+                                                     hours_delta=0,
+                                                     minutes_delta=30,
+                                                     seconds_delta=0,
+                                                     milliseconds_delta=0,
+                                                     microseconds_delta=0))
+
+        # Print one Blank Lines, for clear Logging
+        print("\n")
+
+        # Dummy Assert Equal for Unittest
+        self.assertEqual(True, True)
+
+    # Test #5 for Generation of Timestamps
+    # Description of the Test Case:
+    # - Generate the current now Date Time, plus a Delta value, in customised format;
+    def test_generate_now_plus_delta_customised_format(self):
+
+        # Print the Heading Logging for the Test #5
+        print("--- Test #5 for Generation of Timestamps ---")
+
+        # Create the Timestamp's Generator
+        timestamp_generator = TimestampGenerator.TimestampGenerator("timestamp_generator")
+
+        # Print the current now Date Time
+        print("Current Now Date Time, plus a Delta (30 minutes) value, in customised format: ",
+              timestamp_generator
+              .get_now_plus_delta_customised_format(weeks_delta=0,
+                                                    days_delta=0,
+                                                    hours_delta=0,
+                                                    minutes_delta=30,
+                                                    seconds_delta=0,
+                                                    milliseconds_delta=0,
+                                                    microseconds_delta=0))
+
+        # Print one Blank Lines, for clear Logging
+        print("\n")
+
+        # Dummy Assert Equal for Unittest
+        self.assertEqual(True, True)
+
+    # Test #6 for Generation of Timestamps
+    # Description of the Test Case:
+    # - Generate the current now Date Time, plus a Delta value, in Timestamp format;
+    def test_generate_now_plus_delta_timestamp(self):
+
+        # Print the Heading Logging for the Test #6
+        print("--- Test #6 for Generation of Timestamps ---")
+
+        # Create the Timestamp's Generator
+        timestamp_generator = TimestampGenerator.TimestampGenerator("timestamp_generator")
+
+        # Print the current now Date Time
+        print("Current Now Date Time, plus a Delta (30 minutes) value, in Timestamp format: ",
+              timestamp_generator.get_now_plus_delta_timestamp(weeks_delta=0,
+                                                               days_delta=0,
+                                                               hours_delta=0,
+                                                               minutes_delta=30,
+                                                               seconds_delta=0,
+                                                               milliseconds_delta=0,
+                                                               microseconds_delta=0))
+
+        # Print one Blank Lines, for clear Logging
+        print("\n")
+
+        # Dummy Assert Equal for Unittest
+        self.assertEqual(True, True)
+
+    # Test #7 for Generation of Timestamps
+    # Description of the Test Case:
     # - Generate 10 Pseudo Random Timestamps, being valid for the next 2 minutes;
     def test_generate_10_pseudo_random_timestamps_valid_for_the_next_2_minutes(self):
+
+        # Print the Heading Logging for the Test #3
+        print("--- Test #7 for Generation of Timestamps ---")
 
         # Create the Timestamp's Generator
         timestamp_generator = TimestampGenerator.TimestampGenerator("timestamp_generator")
@@ -57,6 +204,9 @@ class TimestampPseudoRandomGeneratorTests(unittest.TestCase):
 
             # Increases the number of current Pseudo Random Timestamps, currently generated
             current_num_pseudo_random_timestamps_generated = current_num_pseudo_random_timestamps_generated + 1
+
+        # Print one Blank Lines, for clear Logging
+        print("\n")
 
         # Dummy Assert Equal for Unittest
         self.assertEqual(True, True)
