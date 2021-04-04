36873bee21f84019fc0bf0b61a36e3a8d2190e41
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-04-04 12:00:59 +0100

    IBM Qiskit's Communication Module added

---
+++ src/ibm_qiskit/node/common/communication/modules/QiskitCommunicationModule.py
@@ -0,0 +1,340 @@
+"""
+Semi-Quantum Conference Key Agreement (SQCKA)
+
+Author:
+- Ruben Andre Barreiro (r.barreiro@campus.fct.unl.pt)
+
+Supervisors:
+- Andre Nuno Souto (ansouto@fc.ul.pt)
+- Antonio Maria Ravara (aravara@fct.unl.pt)
+
+Acknowledgments:
+- Paulo Alexandre Mateus (pmat@math.ist.utl.pt)
+"""
+
+# Import required Enumerations and Constants
+
+# Import the Communication Module Type from the IBM_Qiskit.Common
+from src.ibm_qiskit.common import CommunicationModuleTypes
+
+# Import the IBM Qiskit's Hybrid Memory from IBM_Qiskit.Node.Common.Memory.Hardware.Hybrid
+from src.ibm_qiskit.node.common.memory.hardware.hybrid import QiskitHybridMemory
+
+# Import the IBM Qiskit's Quantum Memory from IBM_Qiskit.Node.Common.Memory.Hardware.Quantum
+from src.ibm_qiskit.node.common.memory.hardware.quantum import QiskitQuantumMemory
+
+# Import the IBM Qiskit's Classical Memory from IBM_Qiskit.Node.Common.Memory.Hardware.Classical
+from src.ibm_qiskit.node.common.memory.hardware.classical import QiskitClassicalMemory
+
+
+# Class of the IBM Qiskit's Communication Hardware Module
+class QiskitCommunicationModule:
+
+    # Constructor of the IBM Qiskit's Communication Hardware Module
+    def __init__(self, communication_module_type_tag):
+
+        # The Tag of the IBM Qiskit's Communication Hardware Type of the IBM Qiskit's Communication Hardware Module
+        self.communication_module_type_tag = communication_module_type_tag
+
+        # Initialise the IBM Qiskit's Quantum Communication with Discrete Variables, by Fiber Optic Interfaces
+        self.qiskit_quantum_communication_discrete_variable_fiber_optic_interfaces = []
+
+        # Initialise the IBM Qiskit's Quantum Communication with Discrete Variables, by Satellite Interfaces
+        self.qiskit_quantum_communication_discrete_variable_satellite_interfaces = []
+
+        # Initialise the IBM Qiskit's Quantum Communication with Continuous Variables, by Fiber Optic Interfaces
+        self.qiskit_quantum_communication_continuous_variable_fiber_optic_interfaces = []
+
+        # Initialise the IBM Qiskit's Quantum Communication with Continuous Variables, by Satellite Interfaces
+        self.qiskit_quantum_communication_continuous_variable_satellite_interfaces = []
+
+        # Initialise the IBM Qiskit's Classical Communication, by Fiber Optic Interface
+        self.qiskit_classical_communication_fiber_optic_interfaces = []
+
+        # Initialise the IBM Qiskit's Classical Communication, by Satellite Interface
+        self.qiskit_classical_communication_satellite_interfaces = []
+
+    # Return the Tag of the IBM Qiskit's Communication Hardware Type of the IBM Qiskit's Communication Hardware Module
+    def get_communication_module_type_tag(self):
+        return self.communication_module_type_tag
+
+    # Add an abstract IBM Qiskit's Communication Hardware to the IBM Qiskit's Communication Hardware Module
+    def add_communication_interface(self, qiskit_communication_interface_to_add):
+
+        # If the IBM Qiskit's Memory Hardware Module is Hybrid,
+        # all the type of Memory Hardware (Hybrid, Quantum and Classical) can be added
+        if self.communication_module_type_tag == CommunicationModuleTypes.QUANTUM_COMMUNICATION_ENUM:
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Hybrid Memory Hardware being given
+            if isinstance(qiskit_memory_to_add, QiskitHybridMemory.QiskitHybridMemory):
+
+                # Add the given abstract IBM Qiskit's Memory Hardware to the IBM Qiskit's Memory Hardware Module
+                self.qiskit_hybrid_memories.append(qiskit_memory_to_add)
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Quantum Memory Hardware being given
+            elif isinstance(qiskit_memory_to_add, QiskitQuantumMemory.QiskitQuantumMemory):
+
+                # Add the given abstract IBM Qiskit's Memory Hardware to the IBM Qiskit's Memory Hardware Module
+                self.qiskit_quantum_memories.append(qiskit_memory_to_add)
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Classical Memory Hardware being given
+            elif isinstance(qiskit_memory_to_add, QiskitClassicalMemory.QiskitClassicalMemory):
+
+                # Add the given abstract IBM Qiskit's Memory Hardware to the IBM Qiskit's Memory Hardware Module
+                self.qiskit_classical_memories.append(qiskit_memory_to_add)
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Unknown Memory Hardware being given
+            else:
+
+                # Raise a Value Error
+                raise ValueError("It is not possible to add an Unknown Hardware Memory!!!")
+
+        # If the IBM Qiskit's Memory Hardware Module is Quantum,
+        # only Quantum Memory Hardware can be added
+        elif self.memory_module_type_tag == MemoryModuleTypes.QUANTUM_MEMORY_ENUM:
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Hybrid Memory Hardware being given
+            if isinstance(qiskit_memory_to_add, QiskitHybridMemory.QiskitHybridMemory):
+
+                # Raise a Value Error, according to the given abstract IBM Qiskit's Memory Hardware
+                raise ValueError("It is not possible to add a Hybrid Memory to a Quantum Memory Hardware Module!!!")
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Quantum Memory Hardware being given
+            elif isinstance(qiskit_memory_to_add, QiskitQuantumMemory.QiskitQuantumMemory):
+
+                # Add the given abstract IBM Qiskit's Memory to the IBM Qiskit's Memory Hardware Module
+                self.qiskit_quantum_memories.append(qiskit_memory_to_add)
+
+            # Check if the abstract IBM Qiskit's Memory is compatible with the type of Memory Hardware,
+            # for the case of a Classical Memory Hardware being given
+            elif isinstance(qiskit_memory_to_add, QiskitClassicalMemory.QiskitClassicalMemory):
+
+                # Raise a Value Error, according to the given abstract IBM Qiskit's Memory Hardware
+                raise ValueError("It is not possible to add a Classical Memory to a Quantum Memory Hardware Module!!!")
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Unknown Memory Hardware being given
+            else:
+
+                # Raise a Value Error
+                raise ValueError("It is not possible to add an Unknown Memory Hardware!!!")
+
+        # If the IBM Qiskit's Memory Hardware Module is Classical,
+        # only Classical Memory Hardware can be added
+        elif self.memory_module_type_tag == MemoryModuleTypes.CLASSICAL_MEMORY_ENUM:
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Hybrid Memory Hardware being given
+            if isinstance(qiskit_memory_to_add, QiskitHybridMemory.QiskitHybridMemory):
+
+                # Raise a Value Error, according to the given abstract IBM Qiskit's Memory Hardware
+                raise ValueError("It is not possible to add a Hybrid Memory to a Quantum Memory Hardware Module!!!")
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Quantum Memory Hardware being given
+            elif isinstance(qiskit_memory_to_add, QiskitQuantumMemory.QiskitQuantumMemory):
+
+                # Raise a Value Error, according to the given abstract IBM Qiskit's Memory
+                raise ValueError("It is not possible to add a Classical Memory to a Quantum Memory Hardware Module!!!")
+
+            # Check if the abstract IBM Qiskit's Memory is compatible with the type of Memory Hardware,
+            # for the case of a Classical Memory Hardware being given
+            elif isinstance(qiskit_memory_to_add, QiskitClassicalMemory.QiskitClassicalMemory):
+
+                # Add the given abstract IBM Qiskit's Memory to the IBM Qiskit's Memory Hardware Module
+                self.qiskit_classical_memories.append(qiskit_memory_to_add)
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Unknown Memory Hardware being given
+            else:
+
+                # Raise a Value Error
+                raise ValueError("It is not possible to add an Unknown Memory Hardware!!!")
+
+        # If the IBM Qiskit's Memory Hardware Module is Unknown,
+        # no Memory Hardware can be added and it will be raised a Value Error
+        else:
+
+            # Raise a Value Error
+            raise ValueError("It is impossible to add any type of Memory Hardware to this Memory Hardware Module, "
+                             "due to an Unknown Hardware type of Memory Hardware Module!!!")
+
+    # Remove an abstract IBM Qiskit's Memory Hardware to the IBM Qiskit's Memory Hardware Module
+    def remove_memory(self, qiskit_memory_to_remove):
+
+        # If the IBM Qiskit's Memory Hardware Module is Hybrid,
+        # all the type of Memory Hardware (Hybrid, Quantum and Classical) can be removed
+        if self.memory_module_type_tag == MemoryModuleTypes.HYBRID_MEMORY_ENUM:
+
+            # Check if the abstract IBM Qiskit's Memory is compatible with the type of Memory Hardware,
+            # for the case of a Hybrid Memory Hardware being given
+            if isinstance(qiskit_memory_to_remove, QiskitHybridMemory.QiskitHybridMemory):
+
+                # Remove the given abstract IBM Qiskit's Memory Hardware to the IBM Qiskit's Memory Hardware Module
+                self.qiskit_hybrid_memories.remove(qiskit_memory_to_remove)
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Quantum Memory Hardware being given
+            elif isinstance(qiskit_memory_to_remove, QiskitQuantumMemory.QiskitQuantumMemory):
+
+                # Remove the given abstract IBM Qiskit's Memory Hardware to the IBM Qiskit's Memory Hardware Module
+                self.qiskit_quantum_memories.remove(qiskit_memory_to_remove)
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Classical Memory Hardware being given
+            elif isinstance(qiskit_memory_to_remove, QiskitClassicalMemory.QiskitClassicalMemory):
+
+                # Remove the given abstract IBM Qiskit's Memory Hardware to the IBM Qiskit's Memory Hardware Module
+                self.qiskit_classical_memories.remove(qiskit_memory_to_remove)
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Unknown Memory Hardware being given
+            else:
+
+                # Raise a Value Error
+                raise ValueError("It is not possible to remove an Unknown Memory Hardware!!!")
+
+        # If the IBM Qiskit's Memory Hardware Module is Quantum,
+        # only Quantum Memory Hardware can be removed
+        elif self.memory_module_type_tag == MemoryModuleTypes.QUANTUM_MEMORY_ENUM:
+
+            # Check if the abstract IBM Qiskit's Memory is compatible with the type of Memory Hardware,
+            # for the case of a Hybrid Memory Hardware being given
+            if isinstance(qiskit_memory_to_remove, QiskitHybridMemory.QiskitHybridMemory):
+
+                # Raise a Value Error, according to the given abstract IBM Qiskit's Memory Hardware
+                raise ValueError("It is not possible to remove a Hybrid Memory to a Quantum Memory Hardware Module!!!")
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Quantum Memory Hardware being given
+            elif isinstance(qiskit_memory_to_remove, QiskitQuantumMemory.QiskitQuantumMemory):
+
+                # Add the given abstract IBM Qiskit's Memory to the IBM Qiskit's Memory Hardware Module
+                self.qiskit_quantum_memories.remove(qiskit_memory_to_remove)
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Classical Memory Hardware being given
+            elif isinstance(qiskit_memory_to_remove, QiskitClassicalMemory.QiskitClassicalMemory):
+
+                # Raise a Value Error, according to the given abstract IBM Qiskit's Memory
+                raise ValueError("It is not possible to remove a Classical Memory to "
+                                 "a Quantum Memory Hardware Module!!!")
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Unknown Memory Hardware being given
+            else:
+
+                # Raise a Value Error
+                raise ValueError("It is not possible to remove an Unknown Memory Hardware!!!")
+
+        # If the IBM Qiskit's Memory Module Hardware is Classical,
+        # only Classical Memory Hardware can be added
+        elif self.memory_module_type_tag == MemoryModuleTypes.CLASSICAL_MEMORY_ENUM:
+
+            # Check if the abstract IBM Qiskit's Memory is compatible with the type of Memory Hardware,
+            # for the case of a Hybrid Memory Hardware being given
+            if isinstance(qiskit_memory_to_remove, QiskitHybridMemory.QiskitHybridMemory):
+
+                # Raise a Value Error, according to the given abstract IBM Qiskit's Memory Hardware
+                raise ValueError("It is not possible to add a Hybrid Memory to a Quantum Memory Hardware Module!!!")
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Quantum Memory Hardware being given
+            elif isinstance(qiskit_memory_to_remove, QiskitQuantumMemory.QiskitQuantumMemory):
+
+                # Raise a Value Error, according to the given abstract IBM Qiskit's Memory
+                raise ValueError("It is not possible to remove a Classical Memory to "
+                                 "a Quantum Memory Hardware Module!!!")
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Classical Memory Hardware being given
+            elif isinstance(qiskit_memory_to_remove, QiskitClassicalMemory.QiskitClassicalMemory):
+
+                # Remove the given abstract IBM Qiskit's Memory to the IBM Qiskit's Memory Module
+                self.qiskit_classical_memories.remove(qiskit_memory_to_remove)
+
+            # Check if the abstract IBM Qiskit's Memory Hardware is compatible with the type of Memory Hardware,
+            # for the case of a Unknown Memory Hardware being given
+            else:
+
+                # Raise a Value Error
+                raise ValueError("It is not possible to remove an Unknown Memory Hardware!!!")
+
+        # If the IBM Qiskit's Memory Module is Unknown,
+        # no Memory Hardware can be added and it will be raised a Value Error
+        else:
+
+            # Raise a Value Error
+            raise ValueError("It is impossible to remove any type of Memory to this Memory Hardware Module, "
+                             "due to an Unknown Hardware type of Memory Hardware Module!!!")
+
+    # Return the IBM Qiskit's Hybrid Memories' Hardware of the IBM Qiskit's Memory Hardware Module
+    def get_qiskit_hybrid_memories(self):
+
+        # If the IBM Qiskit's Memory Module Hardware is Hybrid,
+        # then it can have IBM Qiskit's Hybrid Memories' Hardware
+        if self.memory_module_type_tag == MemoryModuleTypes.HYBRID_MEMORY_ENUM:
+
+            # Return the IBM Qiskit's Hybrid Memories' Hardware
+            return self.qiskit_hybrid_memories
+
+        # If the IBM Qiskit's Memory Module Hardware is not Hybrid,
+        # then it cannot have IBM Qiskit's Hybrid Memories' Hardware
+        else:
+
+            # Raise a Value Error
+            raise ValueError("Only Hybrid Memory Modules' Hardware can have Hybrid Memory Hardware!!!")
+
+    # Return the IBM Qiskit's Quantum Memories' Hardware of the IBM Qiskit's Memory Hardware Module
+    def get_qiskit_quantum_memories(self):
+
+        # If the IBM Qiskit's Memory Module Hardware is Hybrid,
+        # then it can have IBM Qiskit's Quantum Memories' Hardware
+        if self.memory_module_type_tag == MemoryModuleTypes.HYBRID_MEMORY_ENUM:
+
+            # Return the IBM Qiskit's Quantum Memories' Hardware
+            return self.qiskit_quantum_memories
+
+        # If the IBM Qiskit's Memory Module Hardware is Quantum,
+        # then it can have IBM Qiskit's Quantum Memories' Hardware
+        elif self.memory_module_type_tag == MemoryModuleTypes.QUANTUM_MEMORY_ENUM:
+
+            # Return the IBM Qiskit's Quantum Memories' Hardware
+            return self.qiskit_quantum_memories
+
+        # If the IBM Qiskit's Memory Module Hardware is not Hybrid neither Quantum,
+        # then it cannot have IBM Qiskit's Quantum Memories' Hardware
+        else:
+
+            # Raise a Value Error
+            raise ValueError("Only Hybrid and Quantum Memory Modules' Hardware can have Quantum Memory Hardware!!!")
+
+    # Return the IBM Qiskit's Classical Memories' Hardware of the IBM Qiskit's Memory Hardware Module
+    def get_qiskit_classical_memories(self):
+
+        # If the IBM Qiskit's Memory Module Hardware is Hybrid,
+        # then it can have IBM Qiskit's Classical Memories' Hardware
+        if self.memory_module_type_tag == MemoryModuleTypes.HYBRID_MEMORY_ENUM:
+
+            # Return the IBM Qiskit's Classical Memories' Hardware
+            return self.qiskit_classical_memories
+
+        # If the IBM Qiskit's Memory Module Hardware is Classical,
+        # then it can have IBM Qiskit's Classical Memories' Hardware
+        elif self.memory_module_type_tag == MemoryModuleTypes.QUANTUM_MEMORY_ENUM:
+
+            # Return the IBM Qiskit's Classical Memories' Hardware
+            return self.qiskit_classical_memories
+
+        # If the IBM Qiskit's Memory Module Hardware is not Hybrid neither Classical,
+        # then it cannot have IBM Qiskit's Classical Memories' Hardware
+        else:
+
+            # Raise a Value Error
+            raise ValueError("Only Hybrid and Classical Memory Modules' Hardware can have Quantum Memory Hardware!!!")
