6721a1c80e1cd93d73fbcd3b7c9c0dcf74485671
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-04-11 00:23:53 +0100

    IBM Qiskit's Quantum Communication, by Fiber Optic and Satellite Interfaces added

---
+++ src/ibm_qiskit/node/common/communication/hardware/quantum/continuous_variable/fiber_optic/QiskitQuantumCommunicationContinuousVariablesFiberOpticInterface.py
@@ -0,0 +1,25 @@
+"""
+Semi-Quantum Conference Key Agreement (SQCKA)
+
+Author:
+- Ruben Andre Barreiro (r.barreiro@campus.fct.unl.pt)
+
+Supervisors:
+- Andre Nuno Souto (ansouto@fc.ul.pt)
+- Antonio Maria Ravara (aravara@fct.unl.pt)
+
+Acknowledgments:
+- Paulo Alexandre Mateus (pmat@math.ist.utl.pt)
+"""
+
+
+# Class for IBM Qiskit's Quantum Communication, using Continuous Variables, by Fiber Optic Interface
+class QiskitQuantumCommunicationContinuousVariablesFiberOpticInterface:
+
+    # Constructor for IBM Qiskit's Quantum Communication, using Continuous Variables, by Fiber Optic Interface
+    def __init__(self, quantum_communication_continuous_variables_fiber_optic_interface_id):
+
+        # Initialise the ID of the IBM Qiskit's Quantum Communication,
+        # using Continuous Variables, by Fiber Optic Interface
+        self.quantum_communication_continuous_variables_fiber_optic_interface_id = \
+            quantum_communication_continuous_variables_fiber_optic_interface_id
---
+++ src/ibm_qiskit/node/common/communication/hardware/quantum/continuous_variable/satellite/QiskitQuantumCommunicationContinuousVariablesSatelliteInterface.py
@@ -0,0 +1,25 @@
+"""
+Semi-Quantum Conference Key Agreement (SQCKA)
+
+Author:
+- Ruben Andre Barreiro (r.barreiro@campus.fct.unl.pt)
+
+Supervisors:
+- Andre Nuno Souto (ansouto@fc.ul.pt)
+- Antonio Maria Ravara (aravara@fct.unl.pt)
+
+Acknowledgments:
+- Paulo Alexandre Mateus (pmat@math.ist.utl.pt)
+"""
+
+
+# Class for IBM Qiskit's Quantum Communication, using Continuous Variables, by Satellite Interface
+class QiskitQuantumCommunicationContinuousVariablesSatelliteInterface:
+
+    # Constructor for IBM Qiskit's Quantum Communication, using Continuous Variables, by Satellite Interface
+    def __init__(self, quantum_communication_continuous_variables_satellite_interface_id):
+
+        # Initialise the ID of the IBM Qiskit's Quantum Communication,
+        # using Continuous Variables, by Satellite Interface
+        self.quantum_communication_continuous_variables_satellite_interface_id = \
+            quantum_communication_continuous_variables_satellite_interface_id
