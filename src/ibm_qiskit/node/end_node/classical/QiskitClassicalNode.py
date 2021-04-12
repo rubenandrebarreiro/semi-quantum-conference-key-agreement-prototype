258e78e165a224e6ec67824a77ace050fbf763c4
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-04-13 00:05:54 +0100

    Some improvements on IBM Qiskit's Classical Node added

--- src/ibm_qiskit/node/end_node/classical/QiskitClassicalNode.py
+++ src/ibm_qiskit/node/end_node/classical/QiskitClassicalNode.py
@@ -12,27 +12,48 @@ Acknowledgments:
 - Paulo Alexandre Mateus (pmat@math.ist.utl.pt)
 """
 
+# Import the IBM Qiskit's Memory Module from
+# the IBM_Qiskit.Node.Common.Memory.Modules
+from src.ibm_qiskit.node.common.memory.modules import QiskitMemoryModule
+
+# Import the IBM Qiskit's Communication Module from
+# the IBM_Qiskit.Node.Common.Communication.Modules
+from src.ibm_qiskit.node.common.communication.modules import QiskitCommunicationModule
+
+# Import the Enumeration for the Memory Module Types from
+# the Common.Enumerations
+from src.common.enumerations import MemoryModuleTypes
+
+# Import the Enumeration for the Communication Module Types from
+# the Common.Enumerations
+from src.common.enumerations import CommunicationModuleTypes
+
 
 # Class of the IBM Qiskit's Classical Node
 class QiskitClassicalNode:
 
-    # Constructor of the IBM Qiskit's Semi-Quantum Node
-    def __init__(self, semi_quantum_node_id, classical_memory_module, classical_communication_module):
+    # Constructor of the IBM Qiskit's Classical Node
+    def __init__(self, classical_node_id):
 
         # The ID of the IBM Qiskit's Classical Node
-        self.semi_quantum_node_id = semi_quantum_node_id
+        self.classical_node_id = classical_node_id
 
         # The Owner Client Party of the IBM Qiskit's Classical Node
         self.owner_client_party = None
 
         # The Boolean Flag, to keep the information about
-        # the IBM Qiskit's Classical Node is currently being owned by some Client/Party
+        # the IBM Qiskit's Classical Node is
+        # currently being owned by some Client/Party
         self.owned_by_client_party = False
 
-        # The Classical Memory Module of the IBM Qiskit's Classical Node
-        self.classical_memory_module = classical_memory_module
+        # The Memory Hardware Module of the IBM Qiskit's Classical Node
+        self.memory_hardware_module = \
+            QiskitMemoryModule.QiskitMemoryModule(MemoryModuleTypes.CLASSICAL_MEMORY_ENUM)
 
-        # TODO - Communication Module
+        # The Communication Hardware Interface of the IBM Qiskit's Classical Node
+        self.communication_hardware_module = \
+            QiskitCommunicationModule\
+            .QiskitCommunicationModule(CommunicationModuleTypes.CLASSICAL_COMMUNICATION_ENUM)
 
     # Attach some Owner Client Party to the IBM Qiskit's Classical Node
     def attach_owner_client_party(self, owner_client_party):
@@ -42,7 +63,8 @@ class QiskitClassicalNode:
         self.owner_client_party = owner_client_party
 
         # Set the Boolean Flag, to keep the information about
-        # the IBM Qiskit's Classical Node is currently being owned by some Client/Party, as True
+        # the IBM Qiskit's Classical Node
+        # is currently being owned by some Client/Party, as True
         self.owned_by_client_party = True
 
     # Release the current Owner Client Party from the IBM Qiskit's Classical Node
@@ -52,17 +74,27 @@ class QiskitClassicalNode:
         self.owner_client_party = None
 
         # Set the Boolean Flag, to keep the information about
-        # the IBM Qiskit's Classical Node is currently being owned by some Client/Party, as False
+        # the IBM Qiskit's Classical Node
+        # is currently being owned by some Client/Party, as False
         self.owned_by_client_party = False
 
     # Return the ID of the IBM Qiskit's Classical Node
-    def get_quantum_node_id(self):
-        return self.semi_quantum_node_id
+    def get_classical_node_id(self):
+        return self.classical_node_id
 
     # Return the Owner Client Party, using the IBM Qiskit's Classical Node
-    def get_quantum_client_name(self):
+    def get_owner_client_party(self):
         return self.owner_client_party
 
-    # Return the Classical Memory Module of the IBM Qiskit's Classical Node
-    def get_classical_memory_module(self):
-        return self.classical_memory_module
+    # Return the Boolean Flag, to keep the information about
+    # the IBM Qiskit's Classical Node is currently being owned by some Client/Party
+    def is_owned_by_client_party(self):
+        return self.owned_by_client_party
+
+    # Return the Memory Hardware Module of the IBM Qiskit's Classical Node
+    def get_memory_hardware_module(self):
+        return self.memory_hardware_module
+
+    # Return the Communication Hardware Interface of the IBM Qiskit's Classical Node
+    def get_communication_hardware_module(self):
+        return self.communication_hardware_module
