e9fd8812bc7bd2b8b12d952727cb0c66b7e347ce
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-04-13 14:05:38 +0100

    More Types of Enumerations for Quantum Entanglements added

--- src/common/enumerations/QuantumEntanglementTypes.py
+++ src/common/enumerations/QuantumEntanglementTypes.py
@@ -15,7 +15,8 @@ Acknowledgments:
 # The Enumerations and Constants
 
 # The possible existing types of Bipartite and Multipartite Quantum Entanglements
-POSSIBLE_QUANTUM_ENTANGLEMENT_TYPES = ["BELL_STATE", "GHZ_STATE", "W_STATE", "RESOURCE_STATE", "GRAPH_STATE"]
+POSSIBLE_QUANTUM_ENTANGLEMENT_TYPES = ["BELL_STATE", "GHZ_STATE", "W_STATE", "DICKE_STATE",
+                                       "RESOURCE_STATE", "GRAPH_STATE", "CLUSTER_STATE"]
 
 # The possible configurations of Bipartite Quantum Entanglements (Bell States)
 POSSIBLE_CONFIGURATIONS_BELL_STATES = ["EPR_PAIR_STATE", "BELL_STATE_PHI_PLUS", "BELL_STATE_PHI_MINUS",
@@ -24,11 +25,29 @@ POSSIBLE_CONFIGURATIONS_BELL_STATES = ["EPR_PAIR_STATE", "BELL_STATE_PHI_PLUS", 
 # The String ID for the Bell State
 BELL_STATE = "BELL_STATE"
 
+# The String ID for the EPR Pair State |ϕ^+⟩
+EPR_PAIR_STATE = "EPR_PAIR_STATE"
+
+# The String ID for the Bell State |ϕ^+⟩
+BELL_STATE_PHI_PLUS = "BELL_STATE_PHI_PLUS"
+
+# The String ID for the Bell State |ϕ^-⟩
+BELL_STATE_PHI_MINUS = "BELL_STATE_PHI_MINUS"
+
+# The String ID for the Bell State |ψ^+⟩
+BELL_STATE_PSI_PLUS = "BELL_STATE_PSI_PLUS"
+
+# The String ID for the Bell State |ψ^-⟩
+BELL_STATE_PSI_MINUS = "BELL_STATE_PSI_MINUS"
+
 # The String ID for the GHZ State
 GHZ_STATE = "GHZ_STATE"
 
 # The String ID for the W State
 W_STATE = "W_STATE"
+
+# The String ID for the Dicke State
+DICKE_STATE = "DICKE_STATE"
 
 # The String ID for the Resource State
 RESOURCE_STATE = "RESOURCE_STATE"
@@ -36,4 +55,5 @@ RESOURCE_STATE = "RESOURCE_STATE"
 # The String ID for the Graph State
 GRAPH_STATE = "GRAPH_STATE"
 
-
+# The String ID for the Cluster State
+CLUSTER_STATE = "CLUSTER_STATE"
