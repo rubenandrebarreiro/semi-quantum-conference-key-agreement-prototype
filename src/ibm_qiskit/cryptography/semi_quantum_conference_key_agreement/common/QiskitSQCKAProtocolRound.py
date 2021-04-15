b1cbfd1725dc2408205dd7aab119c5b73e2a7e25
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-04-15 19:20:38 +0100

    Some changes and improvements on the IBM Qiskit's Protocol Round added

--- src/ibm_qiskit/cryptography/semi_quantum_conference_key_agreement/common/QiskitSQCKAProtocolRound.py
+++ src/ibm_qiskit/cryptography/semi_quantum_conference_key_agreement/common/QiskitSQCKAProtocolRound.py
@@ -17,7 +17,19 @@ Acknowledgments:
 class QiskitSQCKAProtocolRound:
 
     # Constructor for IBM Qiskit's Round for the Semi-Quantum Conference Key Agreement (SQCKA) Protocol
-    def __init__(self, num_current_round, quantum_circuit, party_owner_times):
-        self.num_current_round = num_current_round
-        self.quantum_circuit = quantum_circuit
+    def __init__(self, num_round, qiskit_quantum_circuit, party_owner_times):
+        self.num_round = num_round
+        self.qiskit_quantum_circuit = qiskit_quantum_circuit
         self.party_owner_times = party_owner_times
+
+    # Return the Number of the Round of the Protocol
+    def get_num_round(self):
+        return self.num_round
+
+    # Return the Quantum Circuit of the Round of the Protocol
+    def get_qiskit_quantum_circuit(self):
+        return self.qiskit_quantum_circuit
+
+    # Return the Party's Owner Times for the Slots of the Rounds of the Protocol
+    def get_party_owner_times(self):
+        return self.party_owner_times
