061535695fd60dc52d6cb1481def7423219cabda
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-04-15 21:28:57 +0100

    Modes for the Communication Interfaces added

---
+++ src/common/enumerations/CommunicationInterfacesModesTypes.py
@@ -0,0 +1,19 @@
+"""
+Semi-Quantum Conference Key Agreement (SQCKA)
+
+Author:
+- Ruben Andre Barreiro (r.barreiro@campus.fct.unl.pt)
+
+Supervisors:
+- Andre Nuno Souto (ansouto@fc.ul.pt)
+- Antonio Maria Ravara (aravara@fct.unl.pt)
+
+Acknowledgments:
+- Paulo Alexandre Mateus (pmat@math.ist.utl.pt)
+"""
+
+# The flag for the Out Port of the Communication Interface
+OUT_PORT = "OUT_PORT"
+
+# The flag for the Out Port of the Communication Interface
+IN_PORT = "IN_PORT"
