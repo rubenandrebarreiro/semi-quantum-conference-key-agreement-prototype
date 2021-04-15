a7ddd58faa1139c3db37b8a21e683d22cdf36893
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-04-15 23:42:46 +0100

    Some IDs for the Interfaces and Medium added

--- src/common/enumerations/CommunicationInterfacesModesTypes.py
+++ src/common/enumerations/CommunicationInterfacesModesTypes.py
@@ -17,3 +17,13 @@ OUT_PORT = "OUT_PORT"
 
 # The flag for the Out Port of the Communication Interface
 IN_PORT = "IN_PORT"
+
+# The ID of the Communication Interface for the Sender
+SENDER_INTERFACE_ID = 0
+
+# The ID of the Communication Interface for
+# the Quantum Communication Channel Medium
+MEDIUM_INTERFACE = 1
+
+# The ID of the Communication Interface for the Receiver
+RECEIVER_INTERFACE_ID = 2
