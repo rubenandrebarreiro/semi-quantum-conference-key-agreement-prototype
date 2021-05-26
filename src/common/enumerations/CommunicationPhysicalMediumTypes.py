100705c4cef603a5353663232e437f9e7bb7b190
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-05-27 00:22:43 +0100

    Enumeration for the Communication's Physical Medium added

---
+++ src/common/enumerations/CommunicationPhysicalMediumTypes.py
@@ -0,0 +1,24 @@
+"""
+Semi-Quantum Conference Key Agreement (SQCKA)
+
+Author:
+- Ruben Andre Barreiro (r.barreiro@campus.fct.unl.pt)
+
+Supervisors:
+- Andre Nuno Souto (ansouto@fc.ul.pt)
+- Antonio Maria Ravara (aravara@fct.unl.pt)
+
+Acknowledgments:
+- Paulo Alexandre Mateus (pmat@math.ist.utl.pt)
+"""
+
+# The Enumerations and Constants
+
+# The possible Communication Physical Medium Types
+POSSIBLE_COMMUNICATION_PHYSICAL_MEDIUM_TYPES = ["FIBER_OPTIC", "SATELLITE"]
+
+# The String ID for the Fiber Optic Communication Physical Medium
+FIBER_OPTIC = "FIBER_OPTIC"
+
+# The String ID for the Satellite Communication Physical Medium
+SATELLITE = "SATELLITE"
